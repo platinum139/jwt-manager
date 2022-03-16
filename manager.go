254f7642f423dfa18d0acd611da6c95cfb54d8fe
platinum139 <platinum139.dev@gmail.com>
2022-03-16 18:18:03 +0200

    Make config fields exported

--- manager.go
+++ manager.go
@@ -20,8 +20,8 @@ type JwtConfig struct {
 }
 
 type Config struct {
-    jwt JwtConfig
-    rds rds.RedisConfig
+    Jwt   JwtConfig
+    Redis rds.RedisConfig
 }
 
 type JwtManager struct {
@@ -34,9 +34,9 @@ type JwtManager struct {
 func (manager JwtManager) GenerateAccessToken(userID uint) (string, error) {
     token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
         Subject:   strconv.Itoa(int(userID)),
-        ExpiresAt: time.Now().Add(time.Duration(manager.config.jwt.AccessTokenMin) * time.Minute).Unix(),
+        ExpiresAt: time.Now().Add(time.Duration(manager.config.Jwt.AccessTokenMin) * time.Minute).Unix(),
     })
-    return token.SignedString([]byte(manager.config.jwt.SecretKey))
+    return token.SignedString([]byte(manager.config.Jwt.SecretKey))
 }
 
 func (manager JwtManager) GenerateRefreshToken() (string, error) {
@@ -51,7 +51,7 @@ func (manager JwtManager) GenerateRefreshToken() (string, error) {
 func (manager JwtManager) ValidateAccessToken(tokenString string) (string, error) {
     token, err := jwt.ParseWithClaims(tokenString, &jwt.StandardClaims{},
         func(token *jwt.Token) (interface{}, error) {
-            return []byte(manager.config.jwt.SecretKey), nil
+            return []byte(manager.config.Jwt.SecretKey), nil
         })
     if ve, ok := err.(*jwt.ValidationError); ok {
         if ve.Errors&jwt.ValidationErrorExpired != 0 {
@@ -78,7 +78,7 @@ func (manager JwtManager) SaveRefreshToken(userId, token string) error {
 }
 
 func NewJwtManager(ctx context.Context, log *log.Logger, config Config) *JwtManager {
-    redisClient := rds.NewRedisManager(ctx, log, &config.rds)
+    redisClient := rds.NewRedisManager(ctx, log, &config.Redis)
     return &JwtManager{
         ctx:         ctx,
         log:         log,
